425545dd0953af610a7a309ab017ed4fef4de3e0
Caelvanost <caelvanost@gmail.com>
2026-08-15 22:19:12 +0200

    Add provider scan rules

--- src/Settings.h
+++ src/Settings.h
@@ -9,7 +9,20 @@ namespace BHS
         std::string region;
         std::string label;
         std::string texture;
+        std::string textureDark;
+        std::string textureFair;
+        std::string sex{ "any" };
         std::string type{ "overlay" };
+    };
+
+    struct ScanRule
+    {
+        std::string directory;
+        std::string region;
+        std::string sex{ "any" };
+        std::string prefix;
+        std::string suffix{ ".dds" };
+        bool pairDarkFair{ false };
     };
 
     struct Provider
@@ -18,6 +31,8 @@ namespace BHS
         std::string name;
         std::string plugin;
         std::vector<OverlayStyle> styles;
+        std::vector<ScanRule> scanRules;
+        bool detected{ false };
     };
 
     struct Settings
@@ -28,6 +43,9 @@ namespace BHS
         static Settings& GetSingleton();
         bool Load();
 
-        std::vector<const OverlayStyle*> GetStylesForRegion(std::string_view region) const;
+        std::vector<const OverlayStyle*> GetStylesForRegion(std::string_view region, std::string_view sex = "any") const;
+
+    private:
+        void ScanProvider(Provider& provider);
     };
 }
