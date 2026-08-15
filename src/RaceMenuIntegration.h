b2ca1f5f58e4a6116ad61a9640d168397a328a8b
Caelvanost <caelvanost@gmail.com>
2026-08-15 22:30:17 +0200

    Expose SKEE runtime interfaces

--- src/RaceMenuIntegration.h
+++ src/RaceMenuIntegration.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "SKEEInterfaces.h"
 
 namespace BHS
 {
@@ -9,7 +10,14 @@ namespace BHS
         bool Initialize();
         bool IsAvailable() const noexcept { return available_; }
 
+        SKEE::IOverlayInterface* Overlay() const noexcept { return overlay_; }
+        SKEE::IOverrideInterface* Override() const noexcept { return override_; }
+        SKEE::IActorUpdateManager* ActorUpdate() const noexcept { return actorUpdate_; }
+
     private:
         bool available_{ false };
+        SKEE::IOverlayInterface* overlay_{ nullptr };
+        SKEE::IOverrideInterface* override_{ nullptr };
+        SKEE::IActorUpdateManager* actorUpdate_{ nullptr };
     };
 }
