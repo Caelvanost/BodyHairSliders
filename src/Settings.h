6cf79e268a831ce11f1f185c1230abaea8813a31
Caelvanost <caelvanost@gmail.com>
2026-08-15 22:11:15 +0200

    Generalize body hair provider settings

--- src/Settings.h
+++ src/Settings.h
@@ -1,22 +1,33 @@
 #pragma once
 
-namespace NBHS
+namespace BHS
 {
     struct OverlayStyle
     {
         std::string id;
+        std::string provider;
+        std::string region;
         std::string label;
         std::string texture;
+        std::string type{ "overlay" };
+    };
+
+    struct Provider
+    {
+        std::string id;
+        std::string name;
+        std::string plugin;
+        std::vector<OverlayStyle> styles;
     };
 
     struct Settings
     {
-        std::vector<OverlayStyle> pubic;
-        std::vector<OverlayStyle> armpits;
-        std::vector<OverlayStyle> butt;
+        std::vector<Provider> providers;
         bool matchHairByDefault{ true };
 
         static Settings& GetSingleton();
         bool Load();
+
+        std::vector<const OverlayStyle*> GetStylesForRegion(std::string_view region) const;
     };
 }
